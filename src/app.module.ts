881a2d3ec6ed9b453d3b9a5d3f83d0a331069eba
devKamleshParihar <sahilparihar16102002@gmail.com>
2024-08-27 21:57:50 +0530

    adding configuration setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,48 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './tasks/Task.Entity';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports :[TasksModule,
-    TypeOrmModule.forRoot({
-      type:"postgres",
-      host:"localhost",
-      port:5432,
-      username:"postgres",
-      password:"postgres",
-      database:"tasks-management",
-      autoLoadEntities:true,
-      entities:[Task],
-      synchronize:true
+  imports :[
+    ConfigModule.forRoot({
+      envFilePath:[`.env.stage.${process.env.STAGE}`]
+    }),
+    TasksModule,
+    TypeOrmModule.forRootAsync({
+      imports:[ConfigModule],
+      inject:[ConfigService],
+      useFactory:async (ConfigService:ConfigService)=>{
+        return {
+          type:"postgres",
+          autoLoadEntities:true,
+          host:'localhost',
+          username:'postgres',
+          password:'postgres',
+          entities:[Task],
+          database:'tasks-management'
+          // entities:[Task],
+          // synchronize:true,
+          // host:ConfigService.get('DB_HOST'),
+          // port:ConfigService.get('DB_PORT'),
+          // username:ConfigService.get('DB_USERNAME'),
+          // password:ConfigService.get('DB_PASSWORD'),
+          // database:ConfigService.get('DB_DATABASE')
+        }
+      }
     }),
     AuthModule
   ]
  
 })
 export class AppModule {}
+
+// type:"postgres",
+//       host:"localhost",
+//       port:5432,
+//       username:"postgres",
+//       password:"postgres",
+//       database:"tasks-management",
+//       autoLoadEntities:true,
+//       entities:[Task],
+//       synchronize:true
